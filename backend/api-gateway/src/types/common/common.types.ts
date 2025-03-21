3b93bcfb361fc417ed7e8ef1c786bd3829eb805b
FrancoDellAguila <francodellaguila@hotmail.com>
2025-03-21 01:08:54 +0100

    a

--- backend/api-gateway/src/types/common/common.types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Tipos comunes utilizados en toda la aplicación
- */
-
-/** Tipo para representar un UUID */
-export type UUID = string;
-
-/** Tipo para representar una fecha ISO */
-export type ISODateString = string; 
